bb9a5e6b6ee6671fef03c4a5113e04fadf577f0a
jlbuild <juliabuildbot@gmail.com>
2026-02-23 13:58:24 +0000

    algoimWrapper_jll build 0.3.2+0

--- src/algoimWrapper_jll.jl
+++ src/algoimWrapper_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("algoimWrapper")
-JLLWrappers.@generate_main_file("algoimWrapper", UUID("3c43aa7b-5398-51f3-8d75-8f051e6faa4d"))
+JLLWrappers.@generate_main_file("algoimWrapper", Base.UUID("3c43aa7b-5398-51f3-8d75-8f051e6faa4d"))
 end  # module algoimWrapper_jll
